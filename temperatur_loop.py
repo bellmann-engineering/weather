57f820714ff24cbd5e18b230ffc80b20f095d721
bellmann-engineering <57789413+bellmann-engineering@users.noreply.github.com>
2021-03-01 17:25:18 +0100

    Update temperatur_loop.py

--- temperatur_loop.py
+++ temperatur_loop.py
@@ -1,11 +1,13 @@
+import time
 import board
 import adafruit_dht
-import time
- 
-dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
+
+dhtDevice = adafruit_dht.DHT22(board.D4)
 
 while True:
-  temperature_c = dhtDevice.temperature
-  print("Die aktuelle Temperatur ist: {:.1f} C".format(temperature_c))
-  
-  time.sleep(2)
+    temperature_c = dhtDevice.temperature
+    print("Die aktuelle Temperatur ist: {:.1f} C".format(temperature_c))
+
+    time.sleep(2.0)
+
+
