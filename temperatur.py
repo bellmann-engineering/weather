a4ab65527e91e517b50770b12af60d7c98a626b1
bellmann-engineering <57789413+bellmann-engineering@users.noreply.github.com>
2021-03-01 17:13:59 +0100

    Update temperatur.py
    Bugfix: DHT sensor not found, check wiring

--- temperatur.py
+++ temperatur.py
@@ -1,11 +1,10 @@
 # Beachte: der Sensor kann nur alle 2s abgefragt werden!
 
+import time
 import board
 import adafruit_dht
- 
-# https://learn.adafruit.com/dht?view=all
-# (pin = 4, type)
-dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
+
+dhtDevice = adafruit_dht.DHT22(board.D4)
 
 temperature_c = dhtDevice.temperature
 print("Die aktuelle Temperatur ist: {:.1f} C".format(temperature_c))
