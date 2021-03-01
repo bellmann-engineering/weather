33c29ea5370081ffa2ef4469e49825b1555c4578
bellmann-engineering <57789413+bellmann-engineering@users.noreply.github.com>
2021-03-01 17:25:37 +0100

    Update temperatur_loop_fehlerbehandlung.py

--- temperatur_loop_fehlerbehandlung.py
+++ temperatur_loop_fehlerbehandlung.py
@@ -1,21 +1,22 @@
+import time
 import board
 import adafruit_dht
-import time
- 
-dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
+
+dhtDevice = adafruit_dht.DHT22(board.D4)
 
 while True:
-  try:
-    temperature_c = dhtDevice.temperature
-    print("Die aktuelle Temperatur ist: {:.1f} C".format(temperature_c))
-    
-   except RuntimeError as error:
+    try:
+        temperature_c = dhtDevice.temperature
+        print("Die aktuelle Temperatur ist: {:.1f} C".format(temperature_c))
+
+    except RuntimeError as error:
         print(error.args[0])
-        time.sleep(2.0)
+        time.sleep(2.0) # im Fehlerfall noch einmal 2s warten
         continue
     except Exception as error:
         dhtDevice.exit()
         raise error
-        
-    time.sleep(2)
-    
+
+    time.sleep(2.0)
+
+
