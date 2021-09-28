95f748c922f8ae001bc51472e91bfc6d446ddd12
bellmann-engineering <57789413+bellmann-engineering@users.noreply.github.com>
2021-09-28 19:50:33 +0200

    Update rest_feed.py

--- rest_feed.py
+++ rest_feed.py
@@ -2,13 +2,14 @@ import requests # python -m pip install requests
 import json
 import time
 
-url = "https://io.adafruit.com/api/v2/JanBoltz/feeds/tempdah/data"
+url = "https://io.adafruit.com/api/v2/kbellmann/feeds/temperatur/data"
+
 
 resp = requests.get(url)
 
 if resp.status_code != 200:
     # nicht ok
-    raise ApiError('GET /airports/ {}'.format(resp.status_code))
+    raise ApiError('GET /temperatur/ {}'.format(resp.status_code))
 
 for item in resp.json():
     epoch = item["created_epoch"]
