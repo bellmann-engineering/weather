c0d6691da866ceacbddf2fb7ae510b21e67402af
bellmann-engineering <57789413+bellmann-engineering@users.noreply.github.com>
2022-02-01 19:14:56 +0100

    Add files via upload

---
+++ temperatur_sending_to_cloud.py
@@ -0,0 +1,20 @@
+from Adafruit_IO import Client # pip3 install adafruit-io
+
+ADAFRUIT_IO_USERNAME = 'kbellmann'
+ADAFRUIT_IO_KEY = 'aio_oBqP91rlunrtx5jXprpbpt0KLmbo'
+
+aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+
+#feed = io.feeds('temperatur')
+
+feed = "temperatur"
+key = "temperatur"
+
+temp_c = "23"
+
+#io.send(feed, temp)
+
+tempfeed = aio.feeds(feed)
+aio.send_data(key, temp_c)
+
+
