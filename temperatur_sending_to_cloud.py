9e9f0dd56a05730730d463f0cff26b6e418617e2
bellmann-engineering <57789413+bellmann-engineering@users.noreply.github.com>
2022-02-02 13:34:35 +0100

    Update temperatur_sending_to_cloud.py

--- temperatur_sending_to_cloud.py
+++ temperatur_sending_to_cloud.py
@@ -1,6 +1,6 @@
 from Adafruit_IO import Client # pip3 install adafruit-io
 
-ADAFRUIT_IO_USERNAME = 'kbellmann'
+ADAFRUIT_IO_USERNAME = 'kbellmann' # CHANGE IT!!!
 ADAFRUIT_IO_KEY = 'aio_oBqP91rlunrtx5jXprpbpt0KLmbo' # CHANGE IT!!!
 
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
