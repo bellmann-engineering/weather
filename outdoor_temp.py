ee8fb37a6112c8d02c184f2c19fbf744044eb8c2
bellmann-engineering <57789413+bellmann-engineering@users.noreply.github.com>
2022-09-21 16:20:26 +0200

    Create outdoor_temp.py

---
+++ outdoor_temp.py
@@ -0,0 +1,17 @@
+import requests # python -m pip install requests
+import time
+
+city = "Wolfsburg"
+token = "c417bfd432d32985a579e4363b63a49f"
+url_weathermap = "http://api.openweathermap.org/data/2.5/weather?q={},de&APPID={}&units=metric".format(city , token)
+
+resp_outdoor = requests.get(url_weathermap)
+
+if resp_outdoor.status_code != 200: # ok
+    raise Exception("GET auf openweathermap war nicht erfolgreich")
+
+outdoor_temperatur = resp_outdoor.json()["main"]["temp"]
+outdoor_epoch = resp_outdoor.json()["dt"]
+lesbare_uhrzeit = time.strftime("%H:%M:%S [%d.%m.%Y]", time.localtime(outdoor_epoch))
+print("Outdoor: {} - {} °C".format(lesbare_uhrzeit, outdoor_temperatur)) 
+
