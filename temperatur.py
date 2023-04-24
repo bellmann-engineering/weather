a293d79baca4b4890135889de5d876d4168a282b
bellmann-engineering <57789413+bellmann-engineering@users.noreply.github.com>
2023-04-24 15:41:46 +0200

    Update temperatur.py

--- temperatur.py
+++ temperatur.py
@@ -4,7 +4,7 @@ import time
 import board
 import adafruit_dht
 
-dhtDevice = adafruit_dht.DHT22(board.D4)
+dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False))
 
 temperature_c = dhtDevice.temperature
 print("Die aktuelle Temperatur ist: {:.1f} C".format(temperature_c))
